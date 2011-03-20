b9567091ec444b7e4234f406728a9a5dd293f4be
jolestar <jolestar@616b2822-ef46-bb9d-e524-1f6b1ca0b730>
2011-03-20 07:41:14 +0000

    test

--- trunk/yaya-analyzer/yaya-analyzer-core/src/test/java/org/apache/lucene/analysis/util/AnalyzerUtils.java
+++ trunk/yaya-analyzer/yaya-analyzer-core/src/test/java/org/apache/lucene/analysis/util/AnalyzerUtils.java
@@ -13,33 +13,25 @@ import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
 /**
  * @author jolestar@gmail.com
  */
-public class AnalyzerUtils
-{
+public class AnalyzerUtils {
 	public static String[] tokensFromAnalysis(Analyzer analyzer, String text)
-			throws IOException
-	{
+			throws IOException {
 		TokenStream stream = analyzer.tokenStream("contents", new StringReader(
 				text));
 		ArrayList<String> tokenList = new ArrayList<String>();
 		TermAttribute term = stream.getAttribute(TermAttribute.class);
-//		TypeAttribute type = (TypeAttribute)stream.getAttribute(TypeAttribute.class);
-		while (true)
-		{
-			if(stream.incrementToken()){
-				tokenList.add(term.term());
-			}else{
-				break;
-			}
+		// TypeAttribute type =
+		// (TypeAttribute)stream.getAttribute(TypeAttribute.class);
+		while (stream.incrementToken()) {
+			tokenList.add(term.term());
 		}
 		return tokenList.toArray(new String[0]);
 	}
 
 	public static void displayTokens(Analyzer analyzer, String text)
-			throws IOException
-	{
+			throws IOException {
 		String[] tokens = tokensFromAnalysis(analyzer, text);
-		for (int i = 0; i < tokens.length; i++)
-		{
+		for (int i = 0; i < tokens.length; i++) {
 			String token = tokens[i];
 			System.out.print("[" + token + "] ");
 		}
